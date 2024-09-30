c586353d7707bd9773138eb1ddd06dd97d92d16c
Sr e Sra Queiroz <welbertq32@gmail.com>
2024-09-30 15:57:11 -0300

    Adicionado estilos simples

--- scripts.js
+++ scripts.js
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
+
         console.log("Jamé");
         let num1 = 15;
         let num2 = 27;
         console.log(num1 + num2)
-    </script>
-</body>
-</html>
